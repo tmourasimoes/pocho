a6c661a1f6cc85a8ebbb07e405d4f513df78b11c
Thiago Simoes <tsimoes@intersystems.com>
2025-06-17 10:31:14 -0300

    main

---
+++ apoio/carga_tabelasmoc.sql
@@ -0,0 +1,26 @@
+-- Active: 1720631686773@@127.0.0.1@3306
+
+
+INSERT INTO demo_obj.TbListarSubEspecialidadesConsulta(CdSubEspecialidade, DsSubEspecialidade)   VALUES('1','Clinica Medica');
+INSERT INTO demo_obj.TbListarSubEspecialidadesConsulta(CdSubEspecialidade, DsSubEspecialidade)   VALUES('2','Clinica Especializada');
+
+
+
+INSERT INTO demo_obj.TbListarEspecialidadesConsulta(CdEspecialidade, DsEspecialidade) VALUES(1,'Clinica Medica');
+INSERT INTO demo_obj.TbListarEspecialidadesConsulta(CdEspecialidade, DsEspecialidade) VALUES(2,'Pediatria');
+
+
+INSERT INTO demo_obj.TbListarLaboratorioExames(Codigo, Descricao) VALUES(1,'Hemograma');
+INSERT INTO demo_obj.TbListarLaboratorioExames(Codigo, Descricao) VALUES(2,'Colesterol');
+
+
+INSERT INTO demo_obj.TbListarImagemExames(Codigo, Descricao) VALUES(1,'Endoscopia');
+INSERT INTO demo_obj.TbListarImagemExames(Codigo, Descricao) VALUES(2,'Ultrassom');
+
+
+
+INSERT INTO demo_obj.TbListarTipoConsulta(CdTipoConsulta, DsTipoConsulta) VALUES(1,'EXAMES');
+INSERT INTO demo_obj.TbListarTipoConsulta(CdTipoConsulta, DsTipoConsulta) VALUES(2,'CIRURGIA');
+INSERT INTO demo_obj.TbListarTipoConsulta(CdTipoConsulta, DsTipoConsulta) VALUES(3,'CONSULTA');
+
+
